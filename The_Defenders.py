ab4a8445db8bc5ca38e344af51498bf015ac7dc3
EVG <evgeniy-kovtyak@mail.ru>
2022-06-07 13:31:13 +0300

    The_Defenders completed, rework of add_units method

--- The_Defenders.py
+++ The_Defenders.py
@@ -27,6 +27,7 @@ class Warrior:
     def __init__(self):
         self.health = 50
         self.attack = 5
+        self.defense = 0
         self.is_alive = True
 
     def check_is_alive(self):
@@ -41,6 +42,14 @@ class Knight(Warrior):
         self.attack = 7
 
 
+class Defender(Warrior):
+    def __init__(self):
+        super().__init__()
+        self.attack = 3
+        self.health = 60
+        self.defense = 2
+
+
 class Rookie(Warrior):
     def __init__(self):
         super().__init__()
@@ -51,18 +60,24 @@ class Rookie(Warrior):
 class Army:
     def __init__(self):
         self.army = list()
+        self.units = {
+            'Knight': Knight,
+            'Warrior': Warrior,
+            'Defender': Defender,
+            'Rookie': Rookie,
+        }
 
     def add_units(self, unit, count):
         for iteration in range(count):
-            if unit.__name__ == 'Knight':
-                self.army.append(Knight())
-            elif unit.__name__ == 'Warrior':
-                self.army.append(Warrior())
+            for key in self.units.keys():
+                if unit.__name__ == key:
+                    self.army.append(self.units[key]())
 
 
 class Battle:
     def fight(self, army1, army2):
         while len(army1.army) != 0 and len(army2.army) != 0:
+            print(army1.army, army2.army)
             for unit1 in army1.army:
                 for unit2 in army2.army:
                     if fight(unit_1=unit1, unit_2=unit2):
@@ -73,21 +88,19 @@ class Battle:
                 if not unit1.is_alive:
                     army1.army.remove(unit1)
             if len(army1.army) == 0:
+                print(0)
                 return False
             elif len(army2.army) == 0:
+                print(1)
                 return True
 
 
 def fight(unit_1, unit_2):
     while unit_1.check_is_alive() and unit_2.check_is_alive():
-        unit_2.health -= unit_1.attack
+        unit_2.health -= (unit_1.attack - unit_2.defense if unit_2.defense < unit_1.attack else 0)
         if unit_2.check_is_alive():
-            unit_1.health -= unit_2.attack
+            unit_1.health -= (unit_2.attack - unit_1.defense if unit_1.defense < unit_2.attack else 0)
     return unit_1.check_is_alive()
-
-
-
-
 
 
 if __name__ == '__main__':
@@ -134,4 +147,3 @@ if __name__ == '__main__':
     assert battle.fight(my_army, enemy_army) == False
     assert battle.fight(army_3, army_4) == True
     print("Coding complete? Let's try tests!")
-
